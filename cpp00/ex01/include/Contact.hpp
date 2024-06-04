7d23d8f76feefa21dfb5729d3c41942a15ed11cd
Nestor Cruz Gambero <ncruz-ga@c2r4s4.42malaga.com>
2024-06-04 16:12:59 +0200

    ex01

---
+++ cpp00/ex01/include/Contact.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 15:10:14 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/06/04 15:13:01 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact{
+	
+	private:
+    	std::name
+    	std::last_name
+    	std::nickname
+    	std::secret
+    	int index
+    public:
+    
+}
---
+++ cpp00/ex01/include/Phonebook.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 17:15:48 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/06/04 15:13:16 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class Phonebook{
+    
+    private:
+        Contacts    contacts[8]
+        int         index;
+    public:
+        
+};
+#endif
