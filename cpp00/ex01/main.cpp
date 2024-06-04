20068e0d2f2dc0993d85112cd26fce39c32bfdcf
Nestor Cruz Gambero <ncruz-ga@c2r4s4.42malaga.com>
2024-06-04 18:06:05 +0200

    comienzo Phonebook

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncruz-ga <ncruz-ga@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 14:59:57 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/06/04 15:14:54 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/06/04 15:28:09 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Phonebook.hpp"
+#include "../include/Phonebook.hpp"
 
 int main(void)
 {
-     
+	Phonebook	agenda;
+	std::string	input;
+
+    while(1)
+	{
+		std::cout << "-- ADD --" << std::endl;
+		std::cout << "-- SEARCH --" << std::endl;
+		std::cout << "-- EXIT --" << std::endl;
+		std::cout << ">";
+		std::cin >> input;
+		std::cout << input << std::endl;
+	}
+	return (0);
 }
