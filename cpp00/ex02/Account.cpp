107fa2b3ff09e363186534365c0ddceab2379773
Nestor Cruz Gambero <ncruz-ga@c2r5s1.42malaga.com>
2024-06-07 18:17:34 +0200

    Comienzo ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,12 +6,54 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:57:48 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/06/07 16:06:53 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/06/07 17:53:31 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+void	getNbWithdrawals()
+{
+	return (_nbWithdrawals);
+}
+
+void	getNbDeposits()
+{
+	return (_nbDeposits);
+}
+
+void	getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+void	getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() <<";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+bool	Account::makeWithdrawal(int wd)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+}
+
+void	Account::makeDeposit(int dep)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount; << ";deposit:" << deposit;
+	std::cout << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+	_nbDeposits += 1;
+	_totalNbDeposits += 1;
+	_amount += deposit;
+	_totalAmount += deposit;
+}
 
 void	Account::_displayTimestamp(void)
 {
