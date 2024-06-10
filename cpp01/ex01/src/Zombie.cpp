88b40cca653cb5c435a3b25e24ace7d82137c9c6
Nestor Cruz Gambero <ncruz-ga@c1r3s2.42malaga.com>
2024-06-10 16:37:32 +0200

    Mini messi

---
+++ cpp01/ex01/src/Zombie.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 16:16:45 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/06/10 16:36:23 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->name << ": Dead" << std::endl; 
+}
+
+void	Zombie::announce()
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie()
+{
+
+}
+
+void	Zombie::set_name(std::string name)
+{
+	this->name = name;
+}
---
+++ cpp01/ex01/src/zombieHorde.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 16:14:24 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/06/10 16:35:29 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Zombie.hpp"
+
+Zombie*	zombieHorde(int N, std::string name)
+{
+	Zombie*	horde = new Zombie[N]();
+
+	for (int i = 0; i < N; i++)
+	{
+		horde[i].set_name(name);
+	}
+	return (horde);
+}
