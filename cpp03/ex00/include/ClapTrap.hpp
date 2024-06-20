818b655500f84e075c8c94f520c592c64d7769cf
Nestor Cruz Gambero <ncruz-ga@c3r9s1.42malaga.com>
2024-06-20 17:27:20 +0200

    Comienzo ex01

--- cpp03/ex00/include/ClapTrap.hpp
+++ cpp03/ex00/include/ClapTrap.hpp
@@ -6,7 +6,28 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:21:55 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/06/20 17:21:56 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/06/20 17:26:41 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class	ClapTrap
+{
+	private:
+		std::string	name;
+		int			Hp;
+		int			Ep;
+		int			Ad
+	public:
+		ClapTrap();
+		~ClapTrap();
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+};
+
+#endif
