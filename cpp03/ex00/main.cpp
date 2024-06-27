80333311056c50d61efb2632f0637348d491a5e6
Nestor Cruz Gambero <ncruz-ga@c2r2s4.42malaga.com>
2024-06-27 16:22:09 +0200

    ex00 completo

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:22:01 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/06/21 13:32:12 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/06/27 16:21:36 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,7 @@ int	main(void)
 	ClapTrap	protagonist("tarnished");
 
 	protagonist.attack("Maliketh");
+	protagonist.takeDamage(9);
+	protagonist.beRepaired(10);
+	protagonist.takeDamage(1);
 }
--- cpp03/ex00/src/ClapTrap.cpp
+++ cpp03/ex00/src/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:21:58 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/06/21 13:44:37 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/06/27 16:19:45 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,34 @@ ClapTrap::ClapTrap()
 	std::cout << "Default constructor called." << std::endl;
 	this->name = "default";
 	this->Hp = 0;
+	std::cout << "Claptrap: default "<< " initial Hp = " << this->Hp << std::endl;
 	this->Ep = 0;
+	std::cout << "Claptrap: default "<< " initial Ep = " << this->Ep << std::endl;
 	this->Ad = 0;
+	std::cout << "Claptrap: default "<< " initial Ad = " << this->Ad << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name)
 {
+	std::cout << "Name constructor called" << std::endl;
 	this->name = name;
 	this->Hp = 10;
+	std::cout << "Claptrap: " << this->name << " initial Hp = " << this->Hp << std::endl;
 	this->Ep = 10;
+	std::cout << "Claptrap: " << this->name << " initial Ep = " << this->Ep << std::endl;
 	this->Ad = 0;
-	std::cout << "Claptrap named " << this->name << " has been created." << std::endl;
+	std::cout << "Claptrap: " << this->name << " initial Ad = " << this->Ad << std::endl;
 }
 ClapTrap::ClapTrap(const ClapTrap& original)
 {
-	std::cout << "Copy constructor has been called." << std::endl
+	std::cout << "Copy constructor has been called." << std::endl;
 	this->name = original.name;
 	this->Hp = original.Hp;
+	std::cout << "Claptrap copy: " << this->name << " initial Hp = " << this->Hp << std::endl;
 	this->Ep = original.Ep;
+	std::cout << "Claptrap copy: " << this->name << " initial Ep = " << this->Ep << std::endl;
 	this->Ad = original.Ad;
+	std::cout << "Claptrap copy: " << this->name << " initial Ad = " << this->Ad << std::endl;
 }
 
 ClapTrap&	ClapTrap::operator = (const ClapTrap& other)
@@ -52,24 +61,48 @@ ClapTrap&	ClapTrap::operator = (const ClapTrap& other)
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << "ClapTrap named " << this->name << " has been destroyed." << std::endl;
+	std::cout << "ClapTrap named: " << this->name << " has been destroyed." << std::endl;
 }
 
 void	ClapTrap::attack(const std::string& target)
 {
-	std::cout << "ClapTrap " << this->name;
-	std::cout << " attacks " << target;
-	std::cout << " causing " << this->Ad << " points of damage!" << std::endl;
+	if (this->Hp && this->Ep)
+	{
+		std::cout << "ClapTrap " << this->name << " attacks " << target << " causing " << this->Ad << " points of damage!" << std::endl;
+		this->Ep -= 1;
+		return ;
+	}
+	if (this->Ep == 0)
+		std::cout << "ClapTrap " << this->name << " don't have energy to attack." << std::endl;
+	if (this->Hp == 0)
+		std::cout << "ClapTrap " << this->name << " is dead, he can't attack." << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
-	this->Hp -= amount;
-	std::cout << "ClapTrap named " << this->name << " has taken " << amount << " point of damage." << std::endl;
+	if (this->Hp > 0)
+	{
+		if (this->Hp >= (int)amount)
+			this->Hp -= amount;
+		else
+			this->Hp -= this->Hp;
+		std::cout << "ClapTrap named " << this->name << " has taken " << amount << " point of damage." << std::endl;
+		return ;
+	}
+	if (this->Hp == 0)
+		std::cout << "ClapTrap named " << this->name << " can't be attacked, he's already dead."<< std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	this->Hp += amount;
-	std::cout << "ClapTrap named " << this->name << " healed " << amount << " point of damage." << std::endl;
+	if (this->Hp > 0 && this->Ep > 0)
+	{
+		this->Hp += amount;
+		std::cout << "ClapTrap named " << this->name << " healed " << amount << " point of damage." << std::endl;
+		return ;
+	}
+	if (this->Ep == 0)
+		std::cout << "ClapTrap named " << this->name << " don't have energy to be repaired." << std::endl;
+	if (this->Hp == 0)
+		std::cout << "ClapTrap named " << this->name << " is dead, he can't be repaired." << std::endl;
 }
