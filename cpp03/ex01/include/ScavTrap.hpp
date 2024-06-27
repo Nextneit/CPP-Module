24d56f8e5645307b2e073defd2ce90c2142501e0
Nestor Cruz Gambero <ncruz-ga@c2r2s4.42malaga.com>
2024-06-27 16:39:25 +0200

    inicio ex01

---
+++ cpp03/ex01/include/ScavTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 16:30:50 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/06/27 16:36:38 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+
+class	ScavTrap
+{
+	private:
+		std::string	name;
+		int			Hp;
+		int			Ep;
+		int			Ad;
+	public:
+}
+
+#endif
