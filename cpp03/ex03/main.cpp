bb371208dd866ef1c96159884f518279259c9fba
Nestor Cruz Gambero <ncruz-ga@c2r3s3.42malaga.com>
2024-07-02 19:11:17 +0200

    Constructor diamon

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,15 +6,15 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:22:01 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/02 18:41:14 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/02 18:53:29 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./include/FragTrap.hpp"
+#include "./include/DiamondTrap.hpp"
 
 int	main(void)
 {
-	FragTrap	protagonist("tarnished");
+	DiamondTrap	protagonist("tarnished");
 
 	protagonist.takeDamage(9);
 	protagonist.beRepaired(10);
--- cpp03/ex03/src/DiamondTrap.cpp
+++ cpp03/ex03/src/DiamondTrap.cpp
@@ -6,9 +6,18 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 18:45:09 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/02 18:45:33 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/02 18:54:12 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/DiamondTrap.hpp"
 
+DiamondTrap::DiamondTrap()
+{
+
+}
+
+DiamondTrap::~DiamondTrap()
+{
+
+}
