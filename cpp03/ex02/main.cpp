498350da2d144a7f7fd4be0df28ee46331435242
Nestor Cruz Gambero <ncruz-ga@c2r3s3.42malaga.com>
2024-07-02 19:11:38 +0200

    Cambio del main

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:22:01 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/02 18:41:14 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/02 18:57:31 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,5 @@ int	main(void)
 {
 	FragTrap	protagonist("tarnished");
 
-	protagonist.takeDamage(9);
-	protagonist.beRepaired(10);
-	protagonist.takeDamage(1);
 	protagonist.highFivesGuys();
 }
