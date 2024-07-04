a93f0511f1ff7eca0570da9fb56a192bedb47ed5
Nestor Cruz Gambero <ncruz-ga@c2r2s6.42malaga.com>
2024-07-04 17:19:20 +0200

    Creacion de las clases

---
+++ cpp04/ex00/include/Animal.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 17:07:42 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/07/04 17:15:41 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+class	Animal
+{
+	protected:
+		std::string	type;
+	public:
+		Animal();
+		~Animal();
+		Animal(const Animal& original);
+		Animal&	operator = (const Animal& other);
+		void	makeSound();
+};
+
+#endif
