a991a0e06ca2f49031feebd8443b81cab8fac3bb
Nestor Cruz Gambero <ncruz-ga@c2r2s3.42malaga.com>
2024-07-05 13:18:08 +0200

    Animal completo

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,13 +6,15 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:18:29 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/05 13:00:13 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/05 13:14:25 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/Animal.hpp"
 #include "./include/Dog.hpp"
 #include "./include/Cat.hpp"
+#include "./include/WrongAnimal.hpp"
+#include "./include/WrongCat.hpp"
 
 int main()
 {
@@ -21,11 +23,19 @@ int main()
 	const Animal* i = new Cat();
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
+	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
+	const WrongAnimal* k = new WrongAnimal();
+	const WrongAnimal* l = new WrongCat();
+	std::cout << k->getType() << " " << std::endl;
+	std::cout << l->getType() << " " << std::endl;
+	k->makeSound();
+	l->makeSound();
 	delete meta;
 	delete j;
 	delete i;
+	delete k;
+	delete l;
 	return (0);
 }
