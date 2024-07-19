77f94d0127f1ebdfb93ee77549833972d0dcd709
Nestor Cruz Gambero <ncruz-ga@c2r9s5.42malaga.com>
2024-07-19 14:59:28 +0200

    Inicio cpp06

---
+++ CPP_Module06/ex00/include/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 14:53:33 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/07/19 14:55:53 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+
+class	ScalarConverter
+{
+	public:
+		void	convert();
+};
+
+#endif
