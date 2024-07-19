c8d8744752d17ade67b6618b219a2483d1d5c82f
Nestor Cruz Gambero <ncruz-ga@c2r9s5.42malaga.com>
2024-07-19 15:27:44 +0200

    main hecho

--- CPP_Module06/ex00/main.cpp
+++ CPP_Module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 14:59:52 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/19 15:03:55 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/19 15:27:02 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	
+	if (argc != 2)
+	{
+		std::cout << "Incorrect number of arguments. Insert 2 arguments." << std::endl;
+		return (1);
+	}
+	ScalarConverter::convert(argv[1]);
+	return (0);
 }
--- CPP_Module06/ex00/src/ScalarConverter.cpp
+++ CPP_Module06/ex00/src/ScalarConverter.cpp
@@ -6,13 +6,13 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 14:54:02 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/19 15:05:48 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/19 15:27:17 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ScalarConverter.hpp"
 
-void	convert()
+void	convert(std::string arg)
 {
 
 }
