7dd1cff932762bf95fe246f674bc44e98c30c667
Nestor Cruz Gambero <ncruz-ga@c2r9s5.42malaga.com>
2024-07-19 16:52:58 +0200

    Constructores hechos

--- CPP_Module06/ex00/src/ScalarConverter.cpp
+++ CPP_Module06/ex00/src/ScalarConverter.cpp
@@ -6,13 +6,32 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 14:54:02 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/19 15:27:17 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/19 16:46:21 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ScalarConverter.hpp"
 
-void	convert(std::string arg)
+ScalarConverter::ScalarConverter()
 {
+}
 
+ScalarConverter::ScalarConverter(ScalarConverter &original)
+{
+	(void)other;
+}
+
+ScalarConverter&	ScalarConverter::operator = (ScalarConverter &other)
+{
+	(void)other;
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
+
+void	ScalarConverter::convert(std::string arg)
+{
+	
 }
