47a94db35f28f4a5af41c221cff7f77315ee8bfe
Nestor Cruz Gambero <ncruz-ga@c3r3s1.42malaga.com>
2024-07-22 18:14:36 +0200

    Planteamiento Base.cpp

--- CPP_Module06/ex02/include/Base.hpp
+++ CPP_Module06/ex02/include/Base.hpp
@@ -6,19 +6,25 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 17:17:31 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/22 17:33:52 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:06:28 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
 
+# include "A.hpp"
+# include "B.hpp"
+# include "C.hpp"
+# include <iostream>
+# include <stdlib.h>
+
 class	Base
 {
 	public:
 		Base();
 		virtual	~Base();
-		virtual	std::string	getType();
+		virtual	std::string	getType() const;
 };
 
 Base	*generate();
--- CPP_Module06/ex02/src/Base.cpp
+++ CPP_Module06/ex02/src/Base.cpp
@@ -6,12 +6,36 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 17:19:25 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/22 17:37:50 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:03:42 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Base.hpp"
 
+Base::Base()
+{
+}
+
 Base::~Base()
 {
+}
+
+std::string	Base::getType() const
+{
+	return ("Base");
+}
+
+Base	*generate()
+{
+
+}
+
+void	identify(Base *p)
+{
+
+}
+
+void	identify(Base &p)
+{
+
 }
