36619f749c4b916fb09fca742ac8b1b7bbf854e3
Nestor Cruz Gambero <ncruz-ga@c3r9s1.42malaga.com>
2024-07-24 12:40:41 +0200

    ex01 completo

---
+++ CPP_Module07/ex01/include/iter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 12:16:08 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/07/24 12:39:39 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template <typename T> void	iter(T *array, int size, void (*f)(T &))
+{
+	for (int i = 0; i < size; i++)
+	{
+		f (array[i]);
+	}
+}
+
+template <typename T> void	print(T &array)
+{
+	std::cout << array << std::endl;
+}
+
+#endif
--- CPP_Module07/ex01/main.cpp
+++ CPP_Module07/ex01/main.cpp
@@ -6,7 +6,27 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 13:23:28 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/23 13:23:29 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/24 12:39:52 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./include/iter.hpp"
+
+int main()
+{
+	std::cout << "ARRAY TYPE INT\n";
+	int arr[] = {1, 2, 3, 4, 5};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	iter(arr, size, print);
+	
+	std::cout << "\nARRAY TYPE STRING\n";
+	std::string arr1[] = {"hola", "adios", "42", "Malaga", "2024"};
+	int size1 = sizeof(arr1) / sizeof(arr1[0]);
+	iter(arr1, size1, print);
+	
+	std::cout << "\nARRAY TYPE DOUBLE\n";
+	double arr2[] = {1.1, 1.2, 1.3, 1.4, 1.5};
+	int size2 = sizeof(arr2) / sizeof(arr2[0]);
+	iter(arr2, size2, print);
+	return 0;
+}
