b2b4aa109ebae4d3062273cd9add51315a9c5b97
Nestor Cruz Gambero <ncruz-ga@c2r10s4.42malaga.com>
2024-07-26 13:07:53 +0200

    Avances main

--- CPP_Module09/ex00/main.cpp
+++ CPP_Module09/ex00/main.cpp
@@ -6,13 +6,17 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:51:33 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/26 12:52:37 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/26 13:02:03 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/BitcoinExhcange.hpp"
 
-int	main()
+int	main(int argc, char **argv)
 {
-
+	if (argc != 2)
+	{
+		std::cerr << "Error: incorrect number of arguments." << std::endl;
+		return (1);
+	}
 }
