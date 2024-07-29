6a23f434d5b59250cc5cf4afbd89e87c16c5ebca
Nestor Cruz Gambero <ncruz-ga@c3r9s5.42malaga.com>
2024-07-29 10:42:49 +0200

    Planteamiento ex00

--- CPP_Module09/ex00/include/BitcoinExchange.hpp
+++ CPP_Module09/ex00/include/BitcoinExchange.hpp
@@ -6,13 +6,18 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:51:24 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/26 12:53:13 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/26 13:34:52 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <iostream>
 
+class	BitcoinExchange
+{
+
+};
 
 #endif
