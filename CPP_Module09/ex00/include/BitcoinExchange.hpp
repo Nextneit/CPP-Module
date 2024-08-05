e6c9b218ca737a567604858363aa7fe6df8025d0
Nestor Cruz Gambero <ncruz-ga@c2r9s2.42malaga.com>
2024-08-05 12:45:44 +0200

    Clase canonica ortodoxa hecha

--- CPP_Module09/ex00/include/BitcoinExchange.hpp
+++ CPP_Module09/ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:51:24 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/26 13:34:52 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/08/05 12:44:43 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <iostream>
+# include <map>
 
 class	BitcoinExchange
 {
-
+	private:
+		std::map<std::string, double> dataBase;
+	public:
+		BitcoinExchange();
+		~BitcoinExchange();
+		BitcoinExchange& operator = (const BitcoinExchange &other);
+		BitcoinExchange(BitcoinExchange &original);
 };
 
 #endif
--- CPP_Module09/ex00/src/BitcoinExchange.cpp
+++ CPP_Module09/ex00/src/BitcoinExchange.cpp
@@ -6,9 +6,28 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:51:21 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/26 12:53:36 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/08/05 12:44:48 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+BitcoinExchange&	BitcoinExchange:: operator = (const BitcoinExchange &other)
+{
+	if (this != &other)
+		this->dataBase = other.dataBase
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange &original)
+{
+	this = original;
+}
 
