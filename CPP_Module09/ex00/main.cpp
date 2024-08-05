f83c02507f1d709605ced37f5f5a9ebf2b2d10c2
Nestor Cruz Gambero <ncruz-ga@c2r9s2.42malaga.com>
2024-08-05 14:30:33 +0200

    Avances ex00

--- CPP_Module09/ex00/main.cpp
+++ CPP_Module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:51:33 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/26 13:02:03 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/08/05 13:00:41 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,6 @@ int	main(int argc, char **argv)
 		std::cerr << "Error: incorrect number of arguments." << std::endl;
 		return (1);
 	}
+	BitcoinExchange	data("data.csv", argv[1])
+	return (0)
 }
--- CPP_Module09/ex00/src/BitcoinExchange.cpp
+++ CPP_Module09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:51:21 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/08/05 12:44:48 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/08/05 14:09:20 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,41 @@ BitcoinExchange&	BitcoinExchange:: operator = (const BitcoinExchange &other)
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange &original)
 {
-	this = original;
+	this->dataBase = original.dataBase;
 }
 
+BitcoinExchange::BitcoinExchange(std::string &data, std::string &input)
+{
+	std::ifstream dataStream;
+	std::ifstream inputStream;
+
+	dataStream.open(data.c_str());
+	inputStream.open(input.c_str());
+	if (!dataStream.is_open())
+	{
+		std::cerr << "Can't open data file." << std::endl;
+		return (1);
+	}
+	if (!inputStream.is_open())
+	{
+		std::cerr << "Can't open input file." << std::endl;
+		return (1);
+	}
+	this->openFile(dataStream, inputStream);
+}
+
+BitcoinExchange::openFile(std::ifstream &dataStream, std::ifstream &inputStream)
+{
+	std::string	str = NULL;
+	
+	while (dataStream.good())
+	{
+		std::getline(dataStream, str);
+		saveDataStream(str, *this)
+	}
+	str = NULL;
+	while (inputStream.good())
+	{
+		std::getline(inputStream, str);
+	}
+}
