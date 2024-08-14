4f30c085009d2655af37d75fdffb33e0ff697434
Nestor Cruz Gambero <ncruz-ga@c3r10s3.42malaga.com>
2024-08-14 17:32:12 +0200

    Arreglos

--- CPP_Module09/ex02/include/PMerge.hpp
+++ CPP_Module09/ex02/include/PMerge.hpp
@@ -6,7 +6,23 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:33:15 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/08/13 16:33:16 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:11:19 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGE_HPP
+# define PMERGE_HPP
+
+# include <iostream>
+# include <vector>
+# include <list>
+
+class	PMerge
+{
+	private:
+
+	public:
+		
+};
+
+#endif
--- CPP_Module09/ex02/main.cpp
+++ CPP_Module09/ex02/main.cpp
@@ -6,7 +6,19 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:33:20 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/08/13 16:33:21 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/08/13 18:13:24 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./include/PMerge.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Error: incorrect number of arguments" << std::endl;
+		return (1);
+	}
+
+	return (0);
+}
